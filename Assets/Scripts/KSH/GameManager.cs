7c2cd30554ecfb5cdbee105286a618894365aa3b
Kosanghyun1 <aazz0200@gmail.com>
2025-05-09 23:37:13 +0900

    [Feat] prefabs maanger system
    prefabs maanger system

--- Assets/Scripts/KSH/GameManager.cs
+++ Assets/Scripts/KSH/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : SingletonInherit<GameManager>
     public StageManager stageManager;
     public GoldManager goldManager; 
     public WorldTime worldTime;
+    //사운드 UI 오브젝트풀링 데이터 리소스 so  
+
 
     private void Start()
     {
