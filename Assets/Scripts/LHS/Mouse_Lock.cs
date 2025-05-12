8d23e180e697ab5717512ef3d0144455a929be0c
lhs0813 <ypd01032@naver.com>
2025-05-13 00:17:38 +0900

    [Fix] Mouse_Cursor , Music_System

---
+++ Assets/Scripts/LHS/Mouse_Lock.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Mouse_Lock : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
