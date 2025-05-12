5ad94d610060556675cded034a607c20ed5501a2
lhs0813 <ypd01032@naver.com>
2025-05-12 16:20:51 +0900

    [Add] CanvasManager_Update

--- Assets/Scripts/LHS/Interact/Trash.cs
+++ Assets/Scripts/LHS/Interact/Trash.cs
@@ -2,8 +2,17 @@
 
 public class Trash : MonoBehaviour, IInteractable
 {
+    CanvasManager _canvasManager;
+     
+    public float duration;
+    public float weight;
+
+
     public void Interact(GameObject interactor, float value)
     {
+        _canvasManager = FindAnyObjectByType<CanvasManager>();
+        _canvasManager._slider.value += weight;
+
         interactor.SetActive(false);
     }
 }
--- Assets/Scripts/LHS/UI/CanvasManager.cs
+++ Assets/Scripts/LHS/UI/CanvasManager.cs
@@ -1,12 +1,13 @@
 ﻿using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
-    
+    public Slider _slider; 
     void Start()
     {
-        
+        _slider = GetComponentInChildren<Slider>();
     }
 
     // Update is called once per frame
